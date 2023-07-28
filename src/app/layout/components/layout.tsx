68ad21a797f49588a78fb3ca72a2c739f3b62359
NhonVo <vothuongtruongnhon2002@gmail.com>
2023-07-29 03:12:36 +0700

    set up ui

---
+++ src/app/layout/components/layout.tsx
@@ -0,0 +1,6 @@
+function Layout(){
+    return (
+        <></>
+    );
+}
+export default Layout;
---
+++ src/app/layout/components/navbar.tsx
@@ -0,0 +1,6 @@
+function Navbar(){
+    return (
+        <></>
+    );
+}
+export default Navbar;
